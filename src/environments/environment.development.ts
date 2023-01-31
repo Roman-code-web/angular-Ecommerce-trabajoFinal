7905dd163a300aa713b1840da3cd93b1791cb60d
Roman-code-web <selenerg.275@gmail.com>
2023-01-30 20:12:00 -0500

    configuracion autenticacion y firestore

---
+++ src/environments/environment.development.ts
@@ -0,0 +1,9 @@
+export const environment = {
+  firebase: {
+    projectId: 'angular-proyectofinal',
+    appId: '1:702350717185:web:60ab9b9f27ca8c2d91eb00',
+    storageBucket: 'angular-proyectofinal.appspot.com',
+    apiKey: 'AIzaSyD_wHXS9njevGMBHUc0Jk-Q2Bzb7tduVVU',
+    authDomain: 'angular-proyectofinal.firebaseapp.com',
+    messagingSenderId: '702350717185',
+  },};
