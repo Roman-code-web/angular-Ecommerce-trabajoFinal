bb61834a2336202270c4a96735147b206468fd16
Roman-code-web <selenerg.275@gmail.com>
2023-02-01 00:20:23 -0500

    validacion de form registro y login

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -7,18 +7,53 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent {
+  mensajeError={
+    email:'',
+    password:''
+  }
+
   formLogin!:FormGroup;
   constructor(private frm: FormBuilder){
     this.formLogin=frm.group({
       email:['',
-      [Validators.required]
+      [
+        Validators.required,
+        Validators.pattern('[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,4}$')
+      ]
       ],
       password:['',
-      [Validators.required]
+      [ Validators.required,
+        Validators.minLength(6)
+      ]
       ] 
     })
   }
   login(){
     console.log(this.formLogin.value)
   }
+
+   //validacion formulario
+  validacion(name: string) {
+    if(this.formLogin.get(name)?.errors && (this.formLogin.get(name)?.touched || this.formLogin.get(name)?.dirty)){
+      if(this.formLogin.get(name)?.errors?.['required']){
+        if(name=='email'){
+          this.mensajeError.email="el campo es requerido"; 
+        }else if (name=='password'){
+          this.mensajeError.password="El campo es requerido";
+        }
+        
+      }else if(this.formLogin.get(name)?.errors?.['minlength']){
+        if(name=='password'){
+          this.mensajeError.password="Mínimo de caracteres 6"; 
+        }
+
+      }else if(this.formLogin.get(name)?.errors?.['pattern']){
+        if(name=='email'){
+          this.mensajeError.email="Email Inválido";
+        }
+      }
+    }
+    return this.formLogin.get(name)?.errors && (this.formLogin.get(name)?.touched || this.formLogin.get(name)?.dirty);
+  }
+
 }
--- src/app/pages/registrar/registrar.component.ts
+++ src/app/pages/registrar/registrar.component.ts
@@ -7,18 +7,52 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
   styleUrls: ['./registrar.component.css']
 })
 export class RegistrarComponent {
+  mensajeError={
+    email:'',
+    password:''
+  }
+
   formRegistrar!:FormGroup;
   constructor(private frmb: FormBuilder){
     this.formRegistrar=frmb.group({
       email:['',
-      [Validators.required]
+      [
+        Validators.required,
+        Validators.pattern('[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,4}$')
+      ]
       ],
       password:['',
-      [Validators.required]
+      [ Validators.required,
+        Validators.minLength(6)
+      ]
       ] 
     });
   }
   registrar(){
     console.log(this.formRegistrar.value)
   }
+  //validacion formulario
+  validacion(name: string) {
+    if(this.formRegistrar.get(name)?.errors && (this.formRegistrar.get(name)?.touched || this.formRegistrar.get(name)?.dirty)){
+      if(this.formRegistrar.get(name)?.errors?.['required']){
+        if(name=='email'){
+          this.mensajeError.email="el campo es requerido"; 
+        }else if (name=='password'){
+          this.mensajeError.password="El campo es requerido";
+        }
+        
+      }else if(this.formRegistrar.get(name)?.errors?.['minlength']){
+        if(name=='password'){
+          this.mensajeError.password="Mínimo de caracteres 6"; 
+        }
+
+      }else if(this.formRegistrar.get(name)?.errors?.['pattern']){
+        if(name=='email'){
+          this.mensajeError.email="Email Inválido";
+        }
+      }
+    }
+    return this.formRegistrar.get(name)?.errors && (this.formRegistrar.get(name)?.touched || this.formRegistrar.get(name)?.dirty);
+  }
+
 }
